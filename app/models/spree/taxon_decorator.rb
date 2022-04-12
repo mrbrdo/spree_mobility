f864e48dd73e76617364d18f4f9d9286bffdf8f9
mrbrdo <mrbrdo@gmail.com>
2022-04-12 17:06:21 +0200

    fix finder methods

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -2,6 +2,7 @@ module Spree::TaxonDecorator
   def self.prepended(base)
     base.translates :name, :description, :meta_title, :meta_description, :meta_keywords,
       :permalink
+    base.friendly_id :permalink, slug_column: :permalink, use: [:history, :mobility]
   end
 
   Spree::Taxon.include SpreeMobility::Translatable
