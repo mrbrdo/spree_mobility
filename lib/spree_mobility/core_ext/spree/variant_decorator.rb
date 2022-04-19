a1912065c5ce4f51c18a57be0abe67d5da95b251
mrbrdo <mrbrdo@gmail.com>
2022-04-19 14:08:23 +0200

    use improved product name matching with fallback support for Variant.search_by_product_name_or_sku, add same support for Variant.product_name_or_sku_cont

--- lib/spree_mobility/core_ext/spree/variant_decorator.rb
+++ lib/spree_mobility/core_ext/spree/variant_decorator.rb
@@ -1,9 +1,18 @@
 module SpreeMobility::CoreExt::Spree
   module VariantDecorator
     module ClassMethods
+      def product_name_or_sku_cont(query)
+        helper =
+          SpreeMobility::TranslationQuery.new(
+            ::Spree::Product.mobility_backend_class(:name))
+
+        helper.add_joins(self.joins(:product)).
+        where(
+          "(LOWER(#{helper.col_name(:name)}) LIKE LOWER(:query)) OR (LOWER(#{::Spree::Variant.table_name}.sku) LIKE LOWER(:query))", query: "%#{query}%").distinct
+      end
+      
       def search_by_product_name_or_sku(query)
-        joins(product: :translations).where("LOWER(#{::Spree::Product::Translation.table_name}.name) LIKE LOWER(:query) OR LOWER(sku) LIKE LOWER(:query)",
-                                            query: "%#{query}%")
+        product_name_or_sku_cont(query)
       end
     end
   end
