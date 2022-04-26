81000e497f4f2726382239c9bdc05a1f108c092b
mrbrdo <mrbrdo@gmail.com>
2022-04-26 19:22:44 +0200

    fix search results sorting

--- lib/spree_mobility/core_ext/spree/products/find_with_mobility_decorator.rb
+++ lib/spree_mobility/core_ext/spree/products/find_with_mobility_decorator.rb
@@ -1,12 +1,24 @@
 module SpreeMobility::CoreExt::Spree::Products::FindWithMobilityDecorator
+  private
   # The issue here is that ordering by translated attr (e.g. name) will add
   # an ORDER BY translations_table.name, but the query has a SELECT DISTINCT,
   # which would require the translations_table.name to be added to the SELECT
-  # Instead of using DISTINCT, we select the ID only and use that as a subquery
-  # in a simple SELECT ... FROM products WHERE products.id IN (SUBQUERY).
-  # Performance-wise should be almost the same, or the same.
-  def execute
-    ::Spree::Product.where(id: super.except(:select).select(:id).distinct(false))
+  # So when the order is added, the appropriate SELECT should be added as well.
+  def ordered(products)
+    case sort_by
+    when 'name-a-z'
+      ordered_name(products, :asc)
+    when 'name-z-a'
+      ordered_name(products, :desc)
+    else
+      super
+    end
+  end
+
+  def ordered_name(products, direction)
+    helper = SpreeMobility::TranslationQuery.new(::Spree::Product.mobility_backend_class(:name))
+    helper.add_joins(products).
+    select(products.arel.projections, "#{helper.col_name(:name)} AS _name").
+    order(Arel.sql(helper.col_name(:name) + ((direction == :desc) ? ' DESC' : '')))
   end
 end
-
