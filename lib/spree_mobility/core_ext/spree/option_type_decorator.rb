fcb87a605b203bf5ebabc543135d2fcab7cc8fc2
mrbrdo <mrbrdo@gmail.com>
2022-04-28 19:16:21 +0200

    fix name for OptionType in admin

--- lib/spree_mobility/core_ext/spree/option_type_decorator.rb
+++ lib/spree_mobility/core_ext/spree/option_type_decorator.rb
@@ -2,10 +2,15 @@ module SpreeMobility::CoreExt::Spree::OptionTypeDecorator
   def self.prepended(base)
     base.include SpreeMobility::Translatable
     SpreeMobility.translates_for base, :name, :presentation
-    
+
     base.translation_class.class_eval do
       validates :name, presence: true, uniqueness: { scope: :locale, case_sensitive: false, allow_blank: true }
       validates :presentation, presence: true
     end
+  end
+
+  # Needed for admin
+  def json_api_columns
+    super + ['name', 'presentation']
   end
 end
