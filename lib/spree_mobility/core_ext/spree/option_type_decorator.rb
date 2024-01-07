f264bb238724595014a30d25db13134073892dda
mrbrdo <mrbrdo@gmail.com>
2024-01-07 22:20:44 +0100

    don't need this
    it never worked anyway, because it should have been a self.method

--- lib/spree_mobility/core_ext/spree/option_type_decorator.rb
+++ lib/spree_mobility/core_ext/spree/option_type_decorator.rb
@@ -7,9 +7,4 @@ module SpreeMobility::CoreExt::Spree::OptionTypeDecorator
       validates :name, presence: true, uniqueness: { scope: :locale, case_sensitive: false, allow_blank: true }
     end
   end
-
-  # Needed for admin
-  def json_api_columns
-    super + ['name', 'presentation']
-  end
 end
